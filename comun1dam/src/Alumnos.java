43ba6ffd81387aaf95fb7124cc8f8b5a609ad251
Daniel <daniellopezlozano@gmail.com>
2024-04-15 13:35:53 +0200

    Proyecto inicial

---
+++ comun1dam/src/Alumnos.java
@@ -0,0 +1,53 @@
+public class Alumnos {
+
+    public void HugoBravoCuesta() {}
+
+    public void IsaacBustamenteMuñoz() {}
+
+    public void AlejandroMolinaCañestro() {}
+
+    public void LuisPeñaCamacho() {}
+
+    public void VictorHugoPinedaBallesteros() {}
+
+    public void PabloSaavedraGarcia() {}
+
+    public void ZufarSharipzianov() {}
+
+    public void PabloZafraJimenez() {}
+
+    public void JesusFernandezJimenez() {}
+
+    public void DanielGarciaMartin() {}
+
+    public void RodrigoGarciaPrieto() {}
+
+    public void RomenGuerraEspínola() {}
+
+    public void GuillemHidalgoMoleon() {}
+
+    public void JesusJimenezPozo() {}
+
+    public void LuciaLaraRojas() {}
+
+    public void ManuelLosaBarrios() {}
+
+    public void JoseOterosRuiz() {}
+
+    public void AlvaroPavonMartinez() {}
+
+    public void EmilioReyesVaquero() {}
+
+    public void RamonJavierRomeroMontilla() {}
+
+    public void MarioSalinasMilena() {}
+
+    public void CynthiaSalinasSalinas() {}
+
+    public void JavierSorianoMoreno() {}
+
+    public void IsmailTbatout() {}
+
+    public void IreneTiradoMontesinos() {}
+}
+
---
+++ comun1dam/src/Principal.java
@@ -0,0 +1,32 @@
+public class Principal {
+
+    public static void main(String[] args) {
+        Alumnos nombres = new Alumnos();
+
+        nombres.HugoBravoCuesta();
+        nombres.IsaacBustamenteMuñoz();
+        nombres.AlejandroMolinaCañestro();
+        nombres.LuisPeñaCamacho();
+        nombres.VictorHugoPinedaBallesteros();
+        nombres.PabloSaavedraGarcia();
+        nombres.ZufarSharipzianov();
+        nombres.PabloZafraJimenez();
+        nombres.JesusFernandezJimenez();
+        nombres.DanielGarciaMartin();
+        nombres.RodrigoGarciaPrieto();
+        nombres.RomenGuerraEspínola();
+        nombres.GuillemHidalgoMoleon();
+        nombres.JesusJimenezPozo();
+        nombres.LuciaLaraRojas();
+        nombres.ManuelLosaBarrios();
+        nombres.JoseOterosRuiz();
+        nombres.AlvaroPavonMartinez();
+        nombres.EmilioReyesVaquero();
+        nombres.RamonJavierRomeroMontilla();
+        nombres.MarioSalinasMilena();
+        nombres.CynthiaSalinasSalinas();
+        nombres.JavierSorianoMoreno();
+        nombres.IsmailTbatout();
+        nombres.IreneTiradoMontesinos();
+    }
+}
